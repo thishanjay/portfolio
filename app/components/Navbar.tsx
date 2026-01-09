f5d64e3b4d6d4cd29e7975a4f87762e07c73a7f1
thishanjay <thishanishere@gmail.com>
2026-01-10 01:01:29 +0530

    add hamburger menu

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,15 +1,24 @@
-import Link from 'next/link'
-import React from 'react'
+"use client";
+
+import React, { useState } from 'react'
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
   return (
-    <div className='m-4 p-4'>
-        <a href="">ThishanJay</a>
-        <div>
+    <div className='m-4 p-4 flex justify-between items-center'>
+        <a href="/" className='text-xl'>ThishanJay</a>
+
+        {/* Hamburger for small screens */}
+        <div className='md:hidden'>
+            <button onClick={() => setIsOpen(!isOpen)} className='text-2xl focus:outline-none'>☰</button>
+        </div>
+
+        <div className={`flex flex-col md:flex-row md:gap-4 absolute md:static top-16 right-4 md:bg-transparent p-4 md:p-0 shadow md:shadow-none transition-all duration-300 ${isOpen ? 'block' : 'hidden'} md:flex`}>
             <a href="about">[About]</a>
-            <a href="about">[Projects]</a>
-            <a href="about">[Contact Me]</a>
-            <a href="about">[About]</a>
+            <a href="skills">[Skills]</a>
+            <a href="projects">[Projects]</a>
+            <a href="contact">[Contact Me]</a>
         </div>
     </div>
   )
