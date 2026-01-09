ac93f60d130fe0a3772d0ff8c97f69270643bfa6
thishanjay <thishanishere@gmail.com>
2026-01-10 00:40:01 +0530

    Add new font

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='m-4 p-4'>
+        <a href="">ThishanJay</a>
+        <div>
+            <a href="about">[About]</a>
+            <a href="about">[Projects]</a>
+            <a href="about">[Contact Me]</a>
+            <a href="about">[About]</a>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { IBM_Plex_Mono as IBMPlexMono } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const ibmPlexMono = IBMPlexMono({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700"], // include all weights you need
 });
 
 export const metadata: Metadata = {
@@ -24,9 +20,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${ibmPlexMono.className} antialiased m-4 font-bold`}>
+        <Navbar /> 
         {children}
       </body>
     </html>
