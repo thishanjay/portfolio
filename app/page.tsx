b25c65ef99b3558531259b3606d5529099da0dcf
thishanjay <thishanishere@gmail.com>
2026-01-15 18:36:41 +0530

    add pages and routes

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>Home
+      <a href=""></a>
+    </div>
   );
 }
---
+++ app/skills/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>skills page</div>
+  )
+}
+export default page
