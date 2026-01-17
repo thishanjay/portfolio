69f71dfb86066a22bea712df8de9c2c40f7b2d5b
thishanjay <thishanishere@gmail.com>
2026-01-18 00:55:46 +0530

    add new buttons

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,11 @@ import Image from "next/image";
 import { IoMdDownload } from "react-icons/io";
 import { MdKeyboardDoubleArrowRight } from "react-icons/md";
 import RoleCarousel from "./components/RoleCarousel";
+import { PiGithubLogoDuotone, PiLinkedinLogoDuotone } from "react-icons/pi";
+import { PiInstagramLogoDuotone } from "react-icons/pi";
+import { ImFacebook } from "react-icons/im";
+import { MdEmail } from "react-icons/md";
+import { SlCallEnd } from "react-icons/sl";
 
 export default function Home() {
   return (
@@ -17,7 +22,7 @@ export default function Home() {
         />
 
         <div className="flex flex-col gap-4 text-center md:text-left">
-          <h1 className="text-4xl font-semibold">Hi, I’m Thishan Jay</h1>
+          <h1 className="text-5xl font-bold">Hi, I’m Thishan Jay</h1>
 
           <RoleCarousel />
 
@@ -33,10 +38,34 @@ export default function Home() {
                 <MdKeyboardDoubleArrowRight />
               </span>
             </button>
-            <button className="border rounded-2xl flex justify-center items-center gap-2 hover:cursor-pointer w-32 p-2 hover:bg-gray-800">
+            <a
+              href="/resume.pdf"
+              download="ThishanJay_Resume.pdf"
+              className="border rounded-2xl flex justify-center items-center gap-2 hover:cursor-pointer w-32 p-2 hover:bg-gray-800"
+            >
               <IoMdDownload />
               Resume
-            </button>
+            </a>
+          </div>
+          <div className="flex gap-4">
+            <a href="" className="icon-btn">
+              <PiGithubLogoDuotone />
+            </a>
+            <a href="" className="icon-btn">
+              <PiLinkedinLogoDuotone />
+            </a>
+            <a href="" className="icon-btn">
+              <PiInstagramLogoDuotone />
+            </a>
+            <a href="" className="icon-btn">
+              <ImFacebook />
+            </a>
+            <a href="" className="icon-btn">
+              <MdEmail />
+            </a>
+            <a href="" className="icon-btn">
+              <SlCallEnd />
+            </a>
           </div>
         </div>
       </div>
